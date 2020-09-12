85262bf2cbf728d32640f04747d5974024057922
KOKI <agiks21@gmail.com>
2020-09-12 20:53:31 +0900

    WEB2_NodeJs_syntax1*2

--- WEB2_NodeJS/main.js
+++ WEB2_NodeJS/main.js
@@ -5,37 +5,66 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    console.log(queryData);
-    var title = queryData.id;
-    if(_url == '/'){
-      title = 'Welcome';
+    var pathname = url.parse(_url, true).pathname;
+
+    if(pathname === '/') {
+      if(queryData.id === undefined) {
+        var title = 'Welcome';
+        fs.readFile(`data/${title}`,'utf-8',function(err,description){
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}
+            </p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      } else {
+        var title = queryData.id;
+        fs.readFile(`data/${title}`,'utf-8',function(err,description){
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}
+            </p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+      });
     }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${title}`,'utf-8',function(err,description){
-      var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${__dirname}${description}
-        </p>
-      </body>
-      </html>
-      `;
-      response.end(template);
-    })
+  } else {
+    response.writeHead(404);
+    response.end('Not Found');
+  }
+  console.log(url.parse(_url, true).pathname);
 });
 app.listen(3000);
---
+++ WEB2_NodeJS/syntax/boolean.js
@@ -0,0 +1,7 @@
+console.log(true);
+console.log(false);
+/*
+below line will cause error
+true = 1;
+true는 예약어기 때문에 데이터를 저장하기 위한 변수로 사용될 수 없다.
+*/
